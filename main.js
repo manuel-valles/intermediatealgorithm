bee810821614fecbd5d1f2cb9e2e396d8e8031f1
manukempo <manukempo@gmail.com>
2017-06-26 08:05:04 +0100

    Roman Numeral Converter

--- main.js
+++ main.js
@@ -43,3 +43,23 @@ $("#buttonDiff").click(function() {
 	var arr2 = str2.split(',');
 	$("#resultDiff").val(diffArray(arr1, arr2));
 });
+
+
+// ROMAN NUMERAL CONVERTER
+function convertToRoman2(num) {
+ 	var romanNum = '',
+	decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+	romans = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+	for (var i = 0; i < decimals.length; i++) {
+		while (num >= decimals[i]) {
+		    romanNum += romans[i];
+		    num -= decimals[i];
+		}
+	}
+    return romanNum;
+}
+
+$("#buttonRoman").click(function() {
+	var number = parseInt($("#number").val());
+	$("#resultRoman").val(convertToRoman2(number));
+});
